b4ec8625b39fbf728563a329ab3cbb340b96534d
Galvann <89554682+Galvann@users.noreply.github.com>
2022-09-02 20:12:14 -0300

    Add files via upload

---
+++ tp1_backend.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practico 1</title>
+</head>
+<body>
+    <?php
+    echo "Hola Mundo";
+    ?>
+    <br>
+    <hr>
+    <?php
+    $saludo = "Hola Mundo";
+    echo $saludo;
+    ?>
+    <br>
+    <hr>
+    <?php
+    $entero1 = 10;
+    $entero2 = 30;
+    echo $entero1 + $entero2;
+    ?>
+    <br>
+    <hr>
+    <?php
+    echo $entero1 - $entero2;
+    ?>
+    <br>
+    <hr>
+    <?php
+    echo $entero1 * $entero2;
+    ?>
+    <br>
+    <hr>
+    <?php
+    echo $entero1 / $entero2;
+    ?>
+    <br>
+    <hr>
+    <?php
+    echo $entero1 % $entero2;
+    ?>
+    <br>
+    <hr>
+    <?php
+    $celsiusAfaren = 20 * 1.8 + 32;
+    echo $celsiusAfaren;
+    ?>
+    <?php
+    $perimetrorectangulo = 18 * 2 + 12 * 2;
+    $arearectangulo =  18 * 12;
+    $perimetrocirculo = 3.14 * 60;
+    $areacirculo = 3.14 * 30 * 30;
+    ?>
+    
+    
+    </body>
+</html>
